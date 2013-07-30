e05e7e784cd12c2dbaed5784c9c00293e1465d87
Don Smith <don.c.smith@gmail.com>
2013-07-29 20:10:31 -0500

    cypher/salt

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '89df98324nr89dh2n3d98239rnt9k4230x9534i9x5i');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '782345023859782350923789050293485032');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
